1adf86ffd2745fe9a48a07db71afd50f0283d044
Dhanraj  Sahu <rajofficials23@gmail.com>
2025-09-17 17:01:49 +0530

    conditional in java
    conditional statement in java

---
+++ Conditional.java
@@ -0,0 +1,34 @@
+public class Conditional {
+    public static void main(String[] args) {
+        /* 
+          Syntax  of if statement : 
+          if(boolean expresssion T or f ){
+          //  body of if 
+          } else {
+           // do this 
+          }
+    
+         */
+
+         int salary = 25000;
+     //    if(salary > 10000){
+    //  salary=salary + 2000;
+  //} else {
+        //    salary = salary +1000;
+            
+        // }
+     //    System.out.println(salary);
+
+    
+     // multipeple is  se if else 
+
+     if( salary > 10000){
+        salary= salary +2000;
+     }
+      else (salary > 20000)
+
+     }
+
+
+
+}
